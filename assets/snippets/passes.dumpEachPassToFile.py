c5f7d9b39f69af40f657280e92e352f787bbc849
AdamTheLesser <adamohern@fb.com>
2016-02-02 14:38:22 -0500

    clean up, document

--- assets/snippets/passes.dumpEachPassToFile.py
+++ assets/snippets/passes.dumpEachPassToFile.py
@@ -1,5 +1,21 @@
 import modo, os.path
 
+"""
+By Adam O'Hern for Mechanical Color
+
+Quick script intended for exporting multi-pass files for render farm use.
+
+Exports a separate file for each active pass in a given pass group. Files are saved
+in the same folder as the parent scene file, and the name of the pass is appended
+to the file name.
+
+To avoid files overwriting themselves when rendered, the script also manually inserts
+the pass name into the output path for each render output. Note that this only applys
+to outputs that have a previously defined output path; others are ignored.
+
+The output pattern can be optionally overridden.
+"""
+
 # Note that backslashes for Windows path names have to be escaped (\\). Mac/Linux paths should be fine as normal (/).
 OUTPUT = 'X:\\path\\to\\destination\\filename'
 PATTERN = '_<output>_<FFFF>'
@@ -7,34 +23,31 @@ PASS_GROUP = 'shots'
 
 scene = modo.Scene()
 group = scene.item(PASS_GROUP)
-
-restorePat = scene.renderItem.channel('outPat').get()
-scene.renderItem.channel('outPat').set(PATTERN)
-
 outputs = [i for i in scene.iterItems() if i.type == 'renderOutput']
+passes = [i for i in group.itemGraph('itemGroups').forward() if i.type == lx.symbol.a_ACTIONCLIP and i.enabled]
 
-for action in [i for i in group.itemGraph('itemGroups').forward() if i.type == lx.symbol.a_ACTIONCLIP and i.enabled]:
-    print '\n\nActivate %s...' % (action.name)
-    action.actionClip.SetActive(1)
+if PATTERN:
+    restorePat = scene.renderItem.channel('outPat').get()
+    scene.renderItem.channel('outPat').set(PATTERN)
 
-    print '\tSetting output paths:'
+for pass_ in passes:
+    pass_.actionClip.SetActive(1)
+
     restoreOut = []
     for output in outputs:
+        if not output.channel('filename').get(): continue
+
         restoreOut.append(output.channel('filename').get())
-        output.channel('filename').set(OUTPUT + '_' + action.name)
+        output.channel('filename').set(OUTPUT + '_' + pass_.name)
 
     path = lx.eval('query sceneservice scene.file ? current')
     splitpath = os.path.splitext(path)
-    path = '%s_%s%s' % (splitpath[0], action.name, splitpath[1])
+    path = '%s_%s%s' % (splitpath[0], pass_.name, splitpath[1])
 
-    print '\tExporting "%s"...' % path
     lx.eval('scene.saveAs {%s} $LXOB true' % path )
 
-    print '\tRestoring output paths...'
     for i, output in enumerate(outputs):
         output.channel('filename').set(restoreOut[i])
 
-    print '\tSuccess.'
-    print '\t++++'
-
-scene.renderItem.channel('outPat').set(restorePat)
+if PATTERN:
+    scene.renderItem.channel('outPat').set(restorePat)
