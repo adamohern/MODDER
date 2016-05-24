57252250bc072f4dadf1d63a6b022cce4490429b
AdamTheLesser <adamohern@fb.com>
2016-05-24 09:33:51 -0400

    fix output path bug

--- assets/snippets/render.updateOutputPaths.py
+++ assets/snippets/render.updateOutputPaths.py
@@ -4,12 +4,16 @@ basepath = os.path.join('Q:',os.sep,'Adam','output')
 
 sceneName = os.path.splitext(modo.Scene().name)
 newpath = os.path.join(basepath, sceneName[0])
+defaultFormat = "openexr"
 
 for i in modo.Scene().items('renderOutput'):
     directory = os.path.join(newpath, i.name)
 
     i.channel('filename').set(os.path.join(directory, sceneName[0]))
+
+    if not i.channel("format").get():
+        i.channel("format").set(defaultFormat)
+
     print directory
     if not os.path.exists(directory):
         os.makedirs(directory)
-
